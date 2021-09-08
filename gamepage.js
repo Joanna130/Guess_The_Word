a23b0af1b1356053881330c96efef2aea273c8d7
Joanna130 <81818250+Joanna130@users.noreply.github.com>
2021-09-08 21:14:43 +0530

    Add files via upload

--- gamepage.js
+++ gamepage.js
@@ -1,9 +1,43 @@
-function addUser(){
-    player1 = document.getElementById("player_1").value;
-    player2 = document.getElementById("player_2").value;
+player1_score = 0;
+player2_score = 0;
+player_1_name = localStorage.getItem("player_1_name");
+player_2_name = localStorage.getItem("player_2_name");
 
-    localStorage.setItem("player1", player1);
-    localStorage.setItem("player2", player2);
+document.getElementById("player_1_name").innerHTML = player_1_name + " : ";
+document.getElementById("player_2_name").innerHTML = player_2_name + " : ";
 
-    window.location = "game_page.html";
+document.getElementById("player_1_score").innerHTML = player1_score ;
+document.getElementById("player_2_score").innerHTML = player2_score ;
+
+document.getElementsByName("player_question").innerHTML = "Question Turn  -" + player_1_name;
+document.getElementsByName("player_answer").innerHTML = "Anser Turn  -" + player_2_name;
+
+
+function send(){
+    get_word = document.getElementsById("word").value;
+    word = get_word.toLowerCase();
+    console.log("word in lowerCase  = " + word);
+
+    charAt1 = word.charAt(1);
+    console.log(charAt);
+
+    length_divide_2 = Math.floor(word.length/2);
+    charAt2 = word.charAt(length_divide_2);
+    console.log(charAt2);
+
+    length_minus_1 = word.length - 1;
+    charAt3 = word.charAt(length_minus_1);
+    console.log(charAt3);
+
+    remove_charAt1 = word.replace(charAt1, "_");
+    remove_charAt2 = remove_charAt1.replace(charAt2, "_");
+    remove_charAt3 = remove_charAt2.replace(charAt3, "_");
+    console.log(remove_charAt3);
+
+    question_word = "<h4 id='word_display'> Q.  "+remove_charAt3+"</h4>";
+    input_box = <br>Answer  :  <input type='text' id='input_cleck_box'>;
+    check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+    row = question_word+input_box + check_button ;
+    document.getElementById("output").innerHTML = row;
+document.getElementById("word").value = "";
 }
